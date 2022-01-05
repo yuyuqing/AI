e9232e13ce94ad8b8f29f0126effa89d24731793
yuyuqing <lvyijiu@icloud.com>
2022-01-05 10:20:53 +0800

    modified

--- k8s_init.sh
+++ k8s_init.sh
@@ -1,4 +1,7 @@
 
+//k8s tutorials begin
+//-----------------------------------------------------------------------------------------------------------------------------
+
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
 
@@ -11,6 +14,10 @@ curl -s https://repo.huaweicloud.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-k
 apt-get update
 apt-get install kubeadm kubelet kubectl
 apt-mark hold kubelet kubeadm kubectl
+
+kubeadm config print init-defaults
+kubeadm config images list
+kubeadm config images list --kubernetes-version=v1.23.1 --image-repository registry.aliyuncs.com/google_containers
 
 kubeadm init \
 --image-repository registry.aliyuncs.com/google_containers \
@@ -31,6 +38,8 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
+kubectl create -f ./calico.yaml
+
 curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz{,.sha256sum}
 sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
 sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
@@ -41,15 +50,46 @@ kubectl get pods -A
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
+kubeadm reset
+systemctl stop kubelet
+systemctl stop docker
+rm -rf /var/lib/cni/
+rm -rf /var/lib/kubelet/*
+rm -rf /etc/cni
+systemctl restart kubelet
+systemctl restart docker
+
+//-----------------------------------------------------------------------------------------------------------------------------
+//k8s tutorials end
+
+
 
 //helm tutorials begin
 //-----------------------------------------------------------------------------------------------------------------------------
 
 helm repo add stable https://charts.helm.sh/stable
 helm repo add azure http://mirror.azure.cn/kubernetes/charts/
+helm repo add cilium https://helm.cilium.io/
 helm repo remove azure
 helm repo list
 helm repo update
+
+helm install cilium cilium/cilium --version 1.11.0 --namespace kube-system
+
+helm install cilium cilium/cilium --version 1.10.2 \
+   --namespace kube-system \
+   --set global.nodeinit.enabled=true \
+   --set global.kubeProxyReplacement=partial \
+   --set global.hostServices.enabled=false \
+   --set global.externalIPs.enabled=true \
+   --set global.nodePort.enabled=true \
+   --set global.hostPort.enabled=true \
+   --set global.pullPolicy=IfNotPresent \
+   --set config.ipam=kubernetes \
+   --set global.hubble.enabled=true \
+   --set global.hubble.relay.enabled=true \
+   --set global.hubble.ui.enabled=true \
+   --set global.hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,http}"
 
 //-----------------------------------------------------------------------------------------------------------------------------
 //helm tutorials end
